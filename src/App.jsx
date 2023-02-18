e55b60aea4f8f90505d8cf21ed96294bb43cc9fe
sankar2403 <sankar2497@gmail.com>
2023-02-18 22:04:02 +0530

    Formik is doned using inbuild component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import OldYotubeForm from './components/OldYoutubeForm/OldYotubeForm'
 import YotubeForm from './components/YoutubeForm/YotubeForm'
 
 const App = () => {
   return (
     <div >
+      {/* <OldYotubeForm /> */}
       <YotubeForm />
     </div>
   )
--- src/components/YoutubeForm/YotubeForm.js
+++ src/components/YoutubeForm/YotubeForm.js
@@ -1,95 +1,140 @@
 import React from 'react'
 import "../YoutubeForm/YoutubeForm.css";
-import { useFormik } from 'formik'
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik'
+import * as Yup from 'yup'
+import TextError from '../OldYoutubeForm/TextError';
 
 
 const initialValues = {
     name: "",
     email: "",
-    channel: ""
+    channel: "",
+    comments: "",
+    address: "",
+    social: {
+        facebook: "",
+        twitter: ""
+    },  // nested object
+    phoneNumbers: ['', ''], // nested array
+    phNumbers: [''] // adding mutiple user inuput using Field Array
 }
 
 const onSubmit = values => {
     console.log("Form Data", values);
 }
 
-const validate = values => {
-    let errors = {}
 
-    if (!values.name) {
-        errors.name = 'Required'
-    }
-
-    if (!values.email) {
-        errors.email = 'Required'
-    }
-    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invaild email format'
-    }
-
-    if (!values.channel) {
-        errors.channel = 'Required'
-    }
-    return errors
-}
+const validationSchema = Yup.object({
+    name: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid Email Format').required('Required'),
+    channel: Yup.string().required('Required')
+})
 
 const YotubeForm = () => {
 
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validate
-    })
-
-    // console.log("Form Values", formik.values)
-    // console.log('Form errors', formik.errors)
-    // console.log('Visited fields',formik.touched)
-
     return (
-        <div className='App'>
-            <form onSubmit={formik.handleSubmit}>
+        <Formik className='App'
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+            <Form>
                 <div className='form-control'>
                     <label htmlFor='name'>Name</label>
-                    <input type='text'
-                        id='name'
-                        name='name'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.name} />
-                    {formik.touched.name && formik.errors.name ?
-                        (<div className="error">{formik.errors.name}</div>) : null}
+                    <Field type='text' id='name' name='name' />
+                    <ErrorMessage name='name' component={TextError} />
                 </div>
 
                 <div className='form-control'>
                     <label htmlFor='email'>E-mail</label>
-                    <input type='email'
-                        id='email'
-                        name='email'
-                        onBlur={formik.handleBlur}
-                        onChange={formik.handleChange}
-                        value={formik.values.email} />
-                    {formik.touched.email && formik.errors.email ?
-                        (<div className="error">{formik.errors.email}</div>) : null}
+                    <Field type='email' id='email' name='email' />
+                    <ErrorMessage name='email'>
+                        {
+                            (errorMsg) => <div className='error'>{errorMsg}</div>
+                        }
+                    </ErrorMessage>
                 </div>
 
                 <div className='form-control'>
                     <label htmlFor='channel'>Channel</label>
-                    <input type='text'
-                        id='channel'
-                        name='channel'
-                        onBlur={formik.handleBlur}
-                        onChange={formik.handleChange}
-                        value={formik.values.channel} />
-                    {formik.touched.channel && formik.errors.channel ?
-                        (<div className="error">{formik.errors.channel}</div>) : null}
+                    <Field type='text' id='channel' name='channel' />
+                    <ErrorMessage name='channel' />
                 </div>
 
-                <button type="submit">Submit</button>
+                <div className='form-control'>
+                    <label htmlFor='comments'>Comments</label>
+                    <Field as='textarea' id='comments' name='comments' />
 
-            </form>
+                </div>
 
-        </div>
+                <div className='form-control'>
+                    <label htmlFor='address'>Address</label>
+                    <Field name='address'>
+                        {
+                            (props) => {
+                                const { field, form, meta } = props
+                                console.log("Render Props", props)
+                                return (
+                                    <div>
+                                        <input type="text" id='address' {...field} />
+                                        {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                                    </div>
+                                )
+                            }
+                        }
+                    </Field>
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='facebook'>Facebook profile</label>
+                    <Field type='text' id='facebook' name='social.facebook' />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='twitter'>Twiter profile</label>
+                    <Field type='text' id='twitter' name='social.twitter' />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='primaryPh'>Primary phone number</label>
+                    <Field type='text' id='primaryPh' name='phoneNumbers[0]' />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='secondaryPh'>Secondary phone number</label>
+                    <Field type='text' id='secondaryPh' name='phoneNumbers[1]' />
+                </div>
+
+                <div className='form-control'>
+                    <label>List of phone numbers</label>
+                    <FieldArray name='phNumbers'>
+                        {
+                            (fieldArrayProps) => {
+                                console.log("fieldArrayProps", fieldArrayProps)
+                                const { push, remove, form } = fieldArrayProps
+                                const { values } = form
+                                const { phNumbers } = values
+                                return <div>
+                                    {
+                                        phNumbers.map((phNumber, index) => {
+                                            return <div key={index}>
+                                            <Field name={`phNumbers[${index}]`} />
+                                            {
+                                                index > 0 &&  <button type='button' onClick={()=> remove(index)}>-</button>
+                                            }
+                                            <button type='button' onClick={()=> push('')}>+</button>
+                                            </div>
+                                        })
+                                    }
+                                </div>
+                            }
+                        }
+                    </FieldArray>
+
+                </div>
+  
+             <button type="submit">Submit</button>
+            </Form>
+        </Formik>
     )
 }
-
 export default YotubeForm
